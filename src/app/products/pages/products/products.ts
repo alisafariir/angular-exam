af703fe43fb73be10bce1520e068bc95e42b627a
Ali Safari <asafari72@gmail.com>
2025-11-30 16:01:07 +0330

    add products page with product listing and service integration

--- src/app/products/pages/products/products.ts
+++ src/app/products/pages/products/products.ts
@@ -1,11 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ProductsService } from '../../products.service';
+import { Product } from '../../../types/products';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.html',
 })
 export class ProductsComponent implements OnInit {
+  getAllProducts = inject(ProductsService);
+  products = signal<Product[]>([]);
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initProducts();
+  }
+
+  initProducts() {
+    this.getAllProducts.getAllProducts().subscribe((res) => {
+      console.log(res);
+      this.products.set((res as any).products);
+    });
+  }
 }
